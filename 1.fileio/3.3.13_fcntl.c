c28d6aa65b5cf80c555605b0f25b9307b22e6796
happymiki <miki_859694663@sina.com>
2017-10-18 19:56:51 +0800

    add fcnt测试

---
+++ 1.fileio/3.3.13_fcntl.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include <unistd.h>
+
+#define FILENAME  "1.txt"
+
+int main(void)
+{
+	int fd1 = -1;
+	int fd2 = -1;
+	
+	fd1 = open(FILENAME,O_RDWR | O_CREAT | O_TRUNC,0644);
+	
+	if (fd1 < 0)
+	{
+		perror("open");
+		return -1;
+	}
+	
+	printf("fd1 = %d.\n",fd1);
+	
+
+	close(1);
+	
+	fd2 = fcntl(fd1,F_DUPFD,0);
+	
+	printf("fd2 = %d.\n",fd2);
+	
+	while (1)
+	{
+		write(fd1,"asd",3);
+		sleep(1);
+		write(fd2,"fgh",3);
+	}
+	
+	close(fd1);
+	
+	return -1;
+}
