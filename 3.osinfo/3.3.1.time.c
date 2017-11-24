24653d6bfad022a18598e1d2b93e99cf325ae1d2
happymiki <miki_859694663@sina.com>
2017-11-24 10:48:17 +0800

    feat:add os information code

--- 3.osinfo/3.3.1.time.c
+++ 3.osinfo/3.3.1.time.c
@@ -1,17 +1,26 @@
 /*************************************************************************
 	> File Name: 3.3.1.time.c
-	> Author: 
+	> Author:miki 
 	> Mail: 
 	> Created Time: Wed 22 Nov 2017 08:14:13 PM CST
  ************************************************************************/
 
-#include<stdio.h>
+#include <stdio.h>
+#include <time.h>
 
 int main(void)
 {
     
-
-
+    time_t tNow = 0;
+    
+    tNow = time(NULL);
+    
+    if (tNow < 0)
+    {
+        perror("time");
+        return -1;
+    }
+    printf("time: %ld.\n",tNow);
 
 
     return 0;
